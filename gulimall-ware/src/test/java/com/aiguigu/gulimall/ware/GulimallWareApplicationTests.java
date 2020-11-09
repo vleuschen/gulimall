272cea6b761c6c76a72f24371ce0278efa8a5055
vleuschen <10719217@qq.com>
2020-11-09 23:33:33 +0800

    1、项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/aiguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiguigu.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
